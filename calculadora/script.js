020c6896c00b73f67f3f4976021e299a5ed8cda2
Arth166 <arth.olive16@gmail.com>
2026-06-01 20:39:17 -0300

    Add files via upload

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,6 +1,13 @@
 function Calcular(){
     let ano_pessoa = document.getElementById("calculo").value;
     let ano_atual = new Date().getFullYear();
-    
-    document.getElementsByClassName("resultado")[0].innerHTML = ano_atual - ano_pessoa;
+
+    if(ano_pessoa < 1920 || ano_pessoa > 2026)
+        alert("coloque um ano valido")
+    else document.getElementsByClassName("resultado")[0].innerHTML = ano_atual - ano_pessoa;
 }
+
+function Calcular2(){
+    let 
+}
+
