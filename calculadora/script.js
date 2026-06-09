90a56331430ddb275c6e136e08aff65c4c3d5ead
Arth166 <arth.olive16@gmail.com>
2026-06-09 19:54:50 -0300

    Add files via upload

--- calculadora/script.js
+++ calculadora/script.js
@@ -14,7 +14,7 @@ function Calcular2() {
     let hoje = new Date()
 
     if (dia <= 0 || dia > 31 || mes == "00" || ano <= 1920 || ano > hoje.getFullYear()){
-        alert("Digite uma data de nascimento valida")
+        alert("para de trollar ai meu, coloca a data direito")
     }
     else {
         let nascimento = new Date(`${ano}/${mes}/${dia}`)
@@ -30,3 +30,44 @@ function Calcular2() {
     }
 }
 
+function Calcular3() {
+    let n1 = document.getElementById("num1").value
+    let op = document.getElementById("opr").value
+    let n2 = document.getElementById("num2").value
+    let resul = 0
+    if ((n1 === "" || n2 === "") && op === "00") {
+    alert("oh great heavens, coloque valores por favor  ;(")
+    return
+    }
+    else if (op === "00") {
+    alert("vai colocar a operação 'selecione a operação' fera? kkkkkkk")
+    return
+    }
+    else if (n1 === "" || n2 === "") {
+    alert("o numero não pode ser vazio")
+    return
+    }
+
+    switch(op) {
+        case "+": resul = Number(n1) + Number(n2); break;
+        case "-": resul = n1 - n2; break;
+        case "*": resul = n1 * n2; break;
+        default: resul = n1 / n2; break;
+    }
+
+    if(resul == 67){
+        let audio = new Audio("./audio/six-seven.mp3")
+        audio.play()
+        document.getElementById("resp3").innerHTML = `o resultado é ${resul} (sixseveennnn)`
+    }
+    else {
+        document.getElementById("resp3").innerHTML = `o resultado é ${resul}`
+
+        if(resul == 42){
+        let audio = new Audio("./audio/moranguete.mp3")
+        audio.play()
+    }
+
+    }
+    
+}
